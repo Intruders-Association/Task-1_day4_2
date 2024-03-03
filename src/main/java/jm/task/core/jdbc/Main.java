5134baaff1e33a4cffd948e105ed8fe85858263a
Aleksandr Bobrov <99945540+723alb@users.noreply.github.com>
2024-03-03 14:01:13 +0300

    Add files via upload

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Freddy", "Krueger", (byte) 40);
+        userService.saveUser("Dexter", "Morgan", (byte) 33);
+        userService.saveUser("Norman", "Bates", (byte) 27);
+        userService.saveUser("Lizzy", "Borden", (byte) 19);
+        userService.toString();
+        userService.removeUserById(1);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,39 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
+
+    static {
+        loadDriver();
+    }
+    private static void loadDriver() {
+        String drvKey = "db.Drv";
+        try {
+            Class.forName(PropertiesUtil.get(drvKey));
+        } catch (ClassNotFoundException e) {
+            logger.log(Level.SEVERE,"ERROR: driver NOT FOUND!!");
+        }
+    }
+    public Util() {}
+    public static Connection connOpen () {
+        String urlKey = "db.url";
+        String userNameKey = "db.username";
+        String passkey = "db.password";
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(PropertiesUtil.get(urlKey),PropertiesUtil.get(userNameKey),PropertiesUtil.get(passkey));
+            logger.log(Level.INFO,"Connection successful");
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE,"ERROR: Connection has lost!!");
+        }
+        return connection;
+    }
 }
